5d4f506bda6b49e8de58ac5b1adb2f83cd14710c
backend dev <developer@leapsail.com.ng>
2022-11-11 09:22:56 -0800

    pw c0mm 30

--- index.js
+++ index.js
@@ -22,6 +22,17 @@ const connectDB = () => {
 
 app.use("/Ardilla/api", authRoute);
 
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
 const PORT = 8000;
 
 app.listen(PORT, () => {
