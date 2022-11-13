b5250db1bc4bb9ca602a61d560766f48539bee57
backend dev <developer@leapsail.com.ng>
2022-11-13 11:19:34 -0800

    sun commit 18

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { register } = require("../controllers/auth");
+const { register, getUser } = require("../controllers/auth");
 const router = express.Router();
 
 router.post("/register", register);
+
+router.get("/user/:id", getUser);
 
 module.exports = router;
