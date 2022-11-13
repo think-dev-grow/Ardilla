3bae80d06393db9fa461264f35a0465fe46b0d96
backend dev <developer@leapsail.com.ng>
2022-11-13 11:14:16 -0800

    sun commit 6

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const Users = require("../models/User");
 const handleError = require("../utils/error");
 const randomize = require("randomatic");
 const Emailer = require("zoho-node-mailer");
+const crypto = require("crypto");
 
 const credentials = {
   username: "developer@leapsail.com.ng",
@@ -23,6 +24,9 @@ const register = async (req, res, next) => {
       subject: "Email verification",
       body: `<p> Please use the OTP code below to complete your accout setting</p>
       <h2>${data.emailToken}</h2>
+      <a href="https://ardilla-web.netlify.app/otp/${data._id}">
+      ${crypto.randomBytes(64).toString("hex")}
+      </a>
      `,
       bodyType: "html",
     };
