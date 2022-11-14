1c84e9dc5f46a33fd981178f9ba1a9a2fa14aea3
backend dev <developer@leapsail.com.ng>
2022-11-14 15:37:53 -0800

    jwt commit 34

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -8,7 +8,7 @@ const verifyToken = (req, res, next) => {
   if (!token) return next(handleError(401, "Unauthorized"));
 
   jwt.verify(token, jwtSecret, (err, user) => {
-    if (err) return next(createError(403, "token not valid"));
+    if (err) return next(handleError(403, "token not valid"));
 
     req.user = user;
     next();
