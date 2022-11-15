56b28c8baf5b36aac9b0449cb9cb8679ed69f58c
backend dev <developer@leapsail.com.ng>
2022-11-15 13:00:55 -0800

    hd commit 12

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -4,15 +4,19 @@ const handleError = require("./error");
 const jwtSecret = "123456789";
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-  if (!token) return next(handleError(401, "Unauthorized"));
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
 
-  jwt.verify(token, jwtSecret, (err, user) => {
-    if (err) return next(handleError(403, "token not valid"));
+    jwt.verify(token, jwtSecret, (err, user) => {
+      if (err) return next(handleError(403, "token not valid"));
 
-    req.user = user;
-    next();
-  });
+      req.user = user;
+      next();
+    });
+  } else {
+    return next(handleError(401, "Unauthorized"));
+  }
 };
 
 module.exports = { verifyToken };
