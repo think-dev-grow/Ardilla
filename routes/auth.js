d56dd2fc2128b474e2182bee150ba97c456a7d06
backend dev <developer@leapsail.com.ng>
2022-11-15 05:11:05 -0800

    pw commit 40

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const {
   getUser,
   completeProfile,
   verifyOTP,
+  resendOTP,
 } = require("../controllers/auth");
 const { verifyToken } = require("../utils/verifyToken");
 const router = express.Router();
@@ -15,5 +16,7 @@ router.get("/user/:id", getUser);
 router.post("/complete-profile/:id", completeProfile);
 
 router.post("/verify-otp", verifyToken, verifyOTP);
+
+router.put("/resend-otp", verifyToken, verifyOTP);
 
 module.exports = router;
