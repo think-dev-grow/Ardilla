2d39512508a0555c0797577dd9ec756b7be62a2a
backend dev <developer@leapsail.com.ng>
2022-11-15 11:50:51 -0800

    pw const 1

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -5,7 +5,7 @@ const jwtSecret = "123456789";
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  if (!token) return next(handleError(401, "Unauthorized"));
+  // if (!token) return next(handleError(401, "Unauthorized"));
 
   jwt.verify(token, jwtSecret, (err, user) => {
     if (err) return next(handleError(403, "token not valid"));
